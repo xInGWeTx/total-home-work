a525289b5fd0a567c39639446aa4fa66f81503ce
InGWet <kozakvlad97@gmail.com>
2022-07-26 16:38:13 +0300

    first part code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
